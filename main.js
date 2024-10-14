fd4a3a853c651990e4178cabf2957ba956b7f18f
yoyo <141518435+YOYOMAII@users.noreply.github.com>
2024-10-14 12:29:55 +0630

    darkmode editing

--- main.js
+++ main.js
@@ -112,22 +112,24 @@ function sendMail() {
 
 }
 
-let darkmode = localStorage.getItem('darkmode')
-const themeSwitch = document.getElementById('theme-switch')
+document.addEventListener('DOMContentLoaded', () => {
+  let darkmode = localStorage.getItem('darkmode');
+  const themeSwitch = document.getElementById('theme-switch');
 
-const enableDarkmode = () => {
-  document.body.classList.add('darkmode')
-  localStorage.setItem('darkmode', 'active')
-}
+  const enableDarkmode = () => {
+    document.body.classList.add('darkmode');
+    localStorage.setItem('darkmode', 'active');
+  };
 
-const disableDarkmode = () => {
-  document.body.classList.remove('darkmode')
-  localStorage.setItem('darkmode', null)
-}
+  const disableDarkmode = () => {
+    document.body.classList.remove('darkmode');
+    localStorage.setItem('darkmode', '');
+  };
 
-if(darkmode === "active") enableDarkmode()
+  if (darkmode === 'active') enableDarkmode();
 
-themeSwitch.addEventListener("click", () => {
-  darkmode = localStorage.getItem('darkmode')
-  darkmode !== "active" ? enableDarkmode() : disableDarkmode()
-})
+  themeSwitch.addEventListener('click', () => {
+    darkmode = localStorage.getItem('darkmode');
+    darkmode !== 'active' ? enableDarkmode() : disableDarkmode();
+  });
+});
